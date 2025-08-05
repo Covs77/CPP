9ad782f5bc0680aa07a54763daf7fadef07d69f5
Covadonga Leguina Roig <cleguina@c3r6s3.42malaga.com>
2025-08-05 18:14:15 +0200

    fixed ex04

--- Module01/ex04/main.cpp
+++ Module01/ex04/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cova <cova@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: cleguina <cleguina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/05 14:39:36 by cova              #+#    #+#             */
-/*   Updated: 2025/08/05 15:12:35 by cova             ###   ########.fr       */
+/*   Updated: 2025/08/05 18:13:48 by cleguina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,13 @@
 #include <sstream>
 #include <string>
 
-
-
-
 std::string my_sed(std::string str, std::string s1, std::string s2)
 {
     if (s1.empty() || s2.empty())
     {
-        std::cerr << "\033[31;1mArgumentos no válidos\033[0m" << std::endl;
+        std::cerr << "\033[31;1mError: Not valid arguments\033[0m" << std::endl;
         return str;
     }
-     
     std::string result;
     size_t i = 0;
     while (i < str.size())
@@ -43,35 +39,45 @@ std::string my_sed(std::string str, std::string s1, std::string s2)
     }
     return result;
 }
-   
+
+std::string getFileName(std::string path)
+{
+    size_t pos = path.find_last_of("/\\");
+    std::string filename = (pos != std::string::npos) ? path.substr(pos + 1) : path;
+    size_t dot = filename.find_last_of('.');
+    if (dot != std::string::npos)
+        filename = filename.substr(0, dot);
+    return filename;
+}
+
 
 int main (int argc, char** argv)
 {
     if (argc < 4)
     {
-        std::cerr << "\033[31;1mError: Se requieren al menos tres argumentos (File, str1, str2).\033[0m" << std::endl;
+        std::cerr << "\033[31;1mError: At least 3 arguments required\n(Example: File, str1, str2)\033[0m" << std::endl;
         return 1;
     }
        std::ifstream file(argv[1]); 
     if (!file.is_open())
     {
-        std::cerr << "Error: No se pudo abrir el archivo " << argv[1] << std::endl;
+        std::cerr << "Error: The file cannot be opened." << argv[1] << std::endl;
         return 1;
     }
+    std::string filename = getFileName(argv[1]);
     std::stringstream buffer;
-    buffer << file.rdbuf(); // Lee todo el contenido del fichero
-    std::string original = buffer.str(); // Guarda en un string
+    buffer << file.rdbuf(); 
+    std::string original = buffer.str(); 
     file.close();
     std::string copia(original);
     copia = my_sed(copia, argv[2], argv[3]);
-     std::ofstream outFile("archivo_modificado.txt");
+    std::ofstream outFile((filename + ".replace").c_str());
     if (outFile.is_open())
     {
         outFile << copia;
         outFile.close();
     }
     else
-    {
-        std::cerr << "No se pudo abrir el archivo para escribir\n";
-    }
+       std::cerr << "Destination file could not be opened\n";
+    return 0;   
 }
