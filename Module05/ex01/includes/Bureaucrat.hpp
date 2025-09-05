f7b7f44053c9015e7a2d3adc770fcb03d2e6bfee
Cova Leguina <cova_leguina@hotmail.com>
2025-09-05 13:51:29 +0200

    ex01

--- Module05/ex01/includes/Bureaucrat.hpp
+++ Module05/ex01/includes/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: cova <cova@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/01 19:04:57 by cova              #+#    #+#             */
-/*   Updated: 2025/09/01 20:19:19 by cova             ###   ########.fr       */
+/*   Updated: 2025/09/05 13:19:17 by cova             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,16 @@
 #define BUREAUCREAT_HPP
 #include  <string>
 #include  <iostream>
+# include "Form.hpp"
 
-
+class Form;
 class Bureaucrat
 {
     private: 
 
-        const std::string _name;
-        int _grade;
-        void _setGrade(int grade);
+        const std::string   _name;
+        int                 _grade;
+        void                _setGrade(int grade);
     
     public:
 
@@ -33,6 +34,7 @@ class Bureaucrat
         Bureaucrat &operator=(Bureaucrat &other);
         std::string const getName() const;
         int getGrade() const;
+        void signForm(Form &form);
         void incrementGrade();
         void decrementGrade();
 
